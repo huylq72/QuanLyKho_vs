169cda299b2dab20ff71b25f459acf5816d864ad
Doan Hieu <hieudx72@gmail.com>
2020-04-04 01:05:06 +0700

    update DataProvider

--- QuanLyKho/Model/DataProvider.cs
+++ QuanLyKho/Model/DataProvider.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,114 @@ namespace QuanLyKho.Model
 {
     public class DataProvider
     {
-        void Demo()
-        {
-            quanlykhoEntities db = new quanlykhoEntities();
+        private static DataProvider instance;
 
+        public static DataProvider Instance 
+        {
+            get { if (instance == null) instance = new DataProvider(); return instance; }
+            private set { instance = value; }
+        }
+        // cách gọi DataProvider.Instance. ....
+        private DataProvider() { }
+
+        private string connectionSTR = "Data Source=.\\sqlexpress;Initial Catalog=QUANLYKHO1;Integrated Security=True";
+
+        public DataTable ExecuteQuery(string query, object[] parameter = null) // trả về datatable
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if(parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach(string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(data);
+
+                connection.Close();
+            }
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query, object[] parameter = null) // trả về số dòng thành công (INSERT)
+        {
+            int data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+
+                }
+
+                data = command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return data;
+        }
+
+        public object ExecuteScalar(string query, object[] parameter = null)  // trả về số lượng COUNT(*)
+        {
+            object data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+
+                }
+
+                data = command.ExecuteScalar();
+
+                connection.Close();
+            }
+            return data;
         }
     }
 }
