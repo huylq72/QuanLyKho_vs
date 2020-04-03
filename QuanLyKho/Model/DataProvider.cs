e249817fbee50484acee3d54a91ee89532d2dbed
baoht1 <baoht@media-one.vn>
2020-04-03 18:44:36 +0700

    firrst

---
+++ QuanLyKho/Model/DataProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho.Model
+{
+    public class DataProvider
+    {
+        void Demo()
+        {
+            quanlykhoEntities db = new quanlykhoEntities();
+
+        }
+    }
+}
