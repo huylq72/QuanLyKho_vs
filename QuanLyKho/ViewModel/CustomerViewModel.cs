7b361f944c0b01970a56c47ad55e23ee26f6866c
NgocLe1001 <ngocthile1001@gmail.com>
2020-04-07 16:58:56 +0700

    update

--- QuanLyKho/ViewModel/CustomerViewModel.cs
+++ QuanLyKho/ViewModel/CustomerViewModel.cs
@@ -1,108 +1,97 @@
-﻿//using QuanLyKho.Model;
-//using System;
-//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows.Input;
+﻿using QuanLyKho.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
-//namespace QuanLyKho.ViewModel
-//{
-//    public class CustomerViewModel : BaseViewModel
-//    {
-//       // private ObservableCollection<Customer> _List;
-//      //  public ObservableCollection<Customer> List { get => _List; set { _List = value; OnPropertyChanged(); } }
+namespace QuanLyKho.ViewModel
+{
+    public class CustomerViewModel : BaseViewModel
+    {
+        private ObservableCollection<KHACHHANG> _List = new ObservableCollection<KHACHHANG>();
+        public ObservableCollection<KHACHHANG> List { get => _List; set { _List = value; OnPropertyChanged(); } }
+        private string _Ten_KH;
+        public string Ten_KH { get => _Ten_KH; set { _Ten_KH = value; OnPropertyChanged(); } }
 
-//      //  private Customer _SelectedItem;
-//        //public Customer SelectedItem
-//        //{
-//        //    get => _SelectedItem;
-//        //    set
-//        //    {
-//        //        _SelectedItem = value;
-//        //        OnPropertyChanged();
-//        //        if (SelectedItem != null)
-//        //        {
-//        //            DisplayName = SelectedItem.DisplayName;
-//        //            Phone = SelectedItem.Phone;
-//        //            Email = SelectedItem.Email;
-//        //            Address = SelectedItem.Address;
-//        //            MoreInfo = SelectedItem.MoreInfo;
-//        //            ContractDate = SelectedItem.ContractDate;
-//        //        }
-//        //    }
-//        //}
+        private string _Dia_Chi;
+        public string Dia_Chi { get => _Dia_Chi; set { _Dia_Chi = value; OnPropertyChanged(); } }
 
-//        private string _DisplayName;
-//        public string DisplayName { get => _DisplayName; set { _DisplayName = value; OnPropertyChanged(); } }
+        private string _Dienthoai;
+        public string Dienthoai { get => _Dienthoai; set { _Dienthoai = value; OnPropertyChanged(); } }
 
+        private string _Email;
+        public string Email { get => _Email; set { _Email = value; OnPropertyChanged(); } }
 
-//        private string _Phone;
-//        public string Phone { get => _Phone; set { _Phone = value; OnPropertyChanged(); } }
+        private KHACHHANG _SelectedItem;
+        public KHACHHANG SelectedItem
+        {
+            get => _SelectedItem;
+            set
+            {
+                _SelectedItem = value;
+                OnPropertyChanged();
+                if (SelectedItem != null)
+                {
+                    Ten_KH = SelectedItem.Ten_KH;
+                    Dia_Chi = SelectedItem.Dia_Chi;
+                    Dienthoai = SelectedItem.Dienthoai;
+                    Email = SelectedItem.Email;
+                }
+            }
+        }
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
 
+        public CustomerViewModel()
+        {
+            string query = "SELECT * from KHACHHANG";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+            foreach (DataRow item in data.Rows)
+            {
+                KHACHHANG KHACHHANG = new KHACHHANG(item);
+                _List.Add(KHACHHANG);
+            }
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(Ten_KH)|| string.IsNullOrEmpty(Dia_Chi)|| string.IsNullOrEmpty(Dienthoai)|| string.IsNullOrEmpty(Email))
+                    return false;
+                string querySearch = "select * from KHACHHANG where Ten_KH = N'" + Ten_KH.ToString() + "' and Dia_Chi= N'" + Dia_Chi.ToString() + "' and Dienthoai= " + Dienthoai.ToString() + " and Email= N'" + Email.ToString() + "'";
+                return true;
 
-//        private string _Address;
-//        public string Address { get => _Address; set { _Address = value; OnPropertyChanged(); } }
+            }, (p) =>
+            {
+                string queryIs = "insert into KHACHHANG(Ten_KH, Dia_Chi, Dienthoai,Email) values(N'" + Ten_KH.ToString() + "', N'"+Dia_Chi.ToString()+"', "+Dienthoai.ToString()+", N'"+Email.ToString()+"')";
+                DataProvider.Instance.ExecuteNonQuery(queryIs);
+                string lastid = "select top 1 * from KHACHHANG ORDER BY Ma_KH DESC";
+                DataTable dataLastId = DataProvider.Instance.ExecuteQuery(lastid);
+                var kh = new KHACHHANG((int)dataLastId.Rows[0]["Ma_KH"], Ten_KH, Dia_Chi,Dienthoai,Email);
+                List.Add(kh);
 
+            });
+            EditCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(Ten_KH)|| string.IsNullOrEmpty(Dia_Chi)|| string.IsNullOrEmpty(Dienthoai)|| string.IsNullOrEmpty(Email) || SelectedItem == null)
+                    return false;
+                string querySearch = "select * from KHACHHANG where Ten_KH = N'" + Ten_KH.ToString() + "' and Dia_Chi= N'" + Dia_Chi.ToString() + "' and Dienthoai= " + Dienthoai.ToString() + " and Email= N'" + Email.ToString() + "'";
+                DataTable dataSearch = DataProvider.Instance.ExecuteQuery(querySearch);
+                if (dataSearch.Rows.Count > 0) return false;
+                return true;
 
-//        private string _Email;
-//        public string Email { get => _Email; set { _Email = value; OnPropertyChanged(); } }
+            }, (p) =>
+            {
+                string queryUd = "Update KHACHHANG set Ten_KH = N'" + Ten_KH.ToString() + "', Dia_Chi = N'" + Dia_Chi.ToString() + "', DienThoai = " + Dienthoai.ToString() + ", Email = N'" + Email.ToString() + "' where Ma_KH = " + SelectedItem.Ma_KH;
+                DataProvider.Instance.ExecuteNonQuery(queryUd);
+                SelectedItem.Ten_KH = Ten_KH;
+                SelectedItem.Dia_Chi = Dia_Chi;
+                SelectedItem.Dienthoai = Dienthoai;
+                SelectedItem.Email = Email;
 
+            });
 
-//        private string _MoreInfo;
-//        public string MoreInfo { get => _MoreInfo; set { _MoreInfo = value; OnPropertyChanged(); } }
-
-
-//        private DateTime? _ContractDate;
-//        public DateTime? ContractDate { get => _ContractDate; set { _ContractDate = value; OnPropertyChanged(); } }
-
-
-//        public ICommand AddCommand { get; set; }
-//        public ICommand EditCommand { get; set; }
-
-//        public CustomerViewModel()
-//        {
-//            List = new ObservableCollection<Customer>(DataProvider.Ins.DB.Customers);
-
-//            AddCommand = new RelayCommand<object>((p) =>
-//            {
-//                return true;
-
-//            }, (p) =>
-//            {
-//                var Customer = new Customer() { DisplayName = DisplayName, Phone = Phone, Address = Address, Email = Email, ContractDate = ContractDate, MoreInfo = MoreInfo };
-
-//                DataProvider.Ins.DB.Customers.Add(Customer);
-//                DataProvider.Ins.DB.SaveChanges();
-
-//                List.Add(Customer);
-//            });
-
-//            EditCommand = new RelayCommand<object>((p) =>
-//            {
-//                if (SelectedItem == null)
-//                    return false;
-
-//                var displayList = DataProvider.Ins.DB.Customers.Where(x => x.Id == SelectedItem.Id);
-//                if (displayList != null && displayList.Count() != 0)
-//                    return true;
-
-//                return false;
-
-//            }, (p) =>
-//            {
-//                var Customer = DataProvider.Ins.DB.Customers.Where(x => x.Id == SelectedItem.Id).SingleOrDefault();
-//                Customer.DisplayName = DisplayName;
-//                Customer.Phone = Phone;
-//                Customer.Address = Address;
-//                Customer.Email = Email;
-//                Customer.ContractDate = ContractDate;
-//                Customer.MoreInfo = MoreInfo;
-//                DataProvider.Ins.DB.SaveChanges();
-
-//                SelectedItem.DisplayName = DisplayName;
-//            });
-//        }
-//    }
-//}
+        }
+    }
+}
---
+++ QuanLyKho/ViewModel/ObjectViewModel.cs
@@ -0,0 +1,101 @@
+﻿using QuanLyKho.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModel
+{
+    public class ObjectViewModel : BaseViewModel
+    {
+        private ObservableCollection<MATHANG> _List = new ObservableCollection<MATHANG>();
+        public ObservableCollection<MATHANG> List { get => _List; set { _List = value; OnPropertyChanged(); } }
+        public int Ma_MatHang { get; set; }
+
+
+       
+        private int _Ma_Dvi { get; set; }
+        public int Ma_Dvi { get => _Ma_Dvi; set { _Ma_Dvi = value; OnPropertyChanged(); } }
+        private string _Ten_MatHang { get; set; }
+        public string Ten_MatHang { get => _Ten_MatHang; set { _Ten_MatHang = value; OnPropertyChanged(); } }
+        private double _Gia_Nhap { get; set; }
+        public double Gia_Nhap { get => _Gia_Nhap; set { _Gia_Nhap = value; OnPropertyChanged(); } }
+
+        private double _Gia_Ban { get; set; }
+        public double Gia_Ban { get => _Gia_Ban; set { _Gia_Ban = value; OnPropertyChanged(); } }
+
+        private MATHANG _SelectedItem;
+        public MATHANG SelectedItem
+        {
+            get => _SelectedItem;
+            set
+            {
+                _SelectedItem = value;
+                OnPropertyChanged();
+                if (SelectedItem != null)
+                {
+                    Ten_MatHang = SelectedItem.Ten_MatHang;
+                    Ma_Dvi = SelectedItem.Ma_Dvi;
+                    Gia_Nhap = SelectedItem.Gia_Nhap;
+                    Gia_Ban = SelectedItem.Gia_Ban;
+                }
+            }
+        }
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        
+        public ObjectViewModel()
+        {
+            
+            string query = "SELECT * from MATHANG";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+            foreach (DataRow item in data.Rows)
+            {
+                MATHANG MATHANG = new MATHANG(item);
+                _List.Add(MATHANG);
+            }
+           
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(Ten_MatHang) || Ma_Dvi<1 || Gia_Nhap<1 || Gia_Ban<1)
+                    return false;
+                string querySearch = "select * from MATHANG where Ten_MatHang = N'" + Ten_MatHang.ToString() + "' and Ma_Dvi= " + Ma_Dvi + " and Gia_Nhap= " +Gia_Nhap + " and Gia_Ban= " + Gia_Ban + "";
+                return true;
+
+            }, (p) =>
+            {
+                string queryIs = "insert into MATHANG(Ten_MatHang, Ma_Dvi, Gia_Nhap,Gia_Ban) values(N'" + Ten_MatHang.ToString() + "', " + Ma_Dvi + ", " + Gia_Nhap + ", " +Gia_Ban + ")";
+                DataProvider.Instance.ExecuteNonQuery(queryIs);
+                string lastid = "select top 1 * from MATHANG ORDER BY Ma_MatHang DESC";
+                DataTable dataLastId = DataProvider.Instance.ExecuteQuery(lastid);
+                var mh = new MATHANG((int)dataLastId.Rows[0]["Ma_MatHang"], Ten_MatHang, (int)Ma_Dvi, (double)Gia_Nhap, (double)Gia_Ban);
+                List.Add(mh);
+
+            });
+            EditCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(Ten_MatHang) || Ma_Dvi < 1 || Gia_Nhap < 1 || Gia_Ban < 1 || SelectedItem == null)
+                    return false;
+                string querySearch = "select * from MATHANG where Ten_MatHang = N'" + Ten_MatHang.ToString() + "' and Ma_Dvi= " + Ma_Dvi + " and Gia_Nhap= " + Gia_Nhap + " and Gia_Ban= " + Gia_Ban + "";
+                DataTable dataSearch = DataProvider.Instance.ExecuteQuery(querySearch);
+                if (dataSearch.Rows.Count > 0) return false;
+                return true;
+
+            }, (p) =>
+            {
+                string queryUd = "Update MAT_HANG set Ten_MatHang = N'" + Ten_MatHang.ToString() + "', Ma_Dvi = " +Ma_Dvi + ", Gia_Nhap = " + Gia_Nhap + ", Gia_Ban = " + Gia_Ban + " where Ma_MatHang = " + SelectedItem.Ma_MatHang;
+                DataProvider.Instance.ExecuteNonQuery(queryUd);
+                SelectedItem.Ten_MatHang = Ten_MatHang;
+                SelectedItem.Ma_Dvi = Ma_Dvi;
+                SelectedItem.Gia_Nhap = Gia_Nhap;
+                SelectedItem.Gia_Ban = Gia_Ban;
+
+            });
+
+        }
+    }
+}
